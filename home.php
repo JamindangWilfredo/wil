6fd64ef6f9d9953a6fef48a19d9e07dfcc201833
JamindangWilfredo <123351984+JamindangWilfredo@users.noreply.github.com>
2024-10-15 18:25:28 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,19 @@
+<section class="container-fluid">
+
+<div class="sec-1 typing-container">
+        <h1>Hello, <br>I'm Wilfredo <br><span class="typing-text">Web Developer</span></h1>
+
+    <div class="sentence">
+        <p><i>Fresh Graduate year of 2023-2024</i></p>
+    </div>
+
+</div>
+
+    <div class="sec-2">
+        <img src="img/aa.png"  alt="Avatar" class="image">
+        <div class="title">
+            Web Developer
+        </div>
+    </div>
+</section>
+
